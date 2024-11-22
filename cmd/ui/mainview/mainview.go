eeb43600bb8af27d7b147f1efa558e23431802a6
tatux <ntatschner@gmail.com>
2024-11-22 00:17:54 +0000

    Adding logger and reorged files

--- cmd/ui/mainview/mainview.go
+++ cmd/ui/mainview/mainview.go
@@ -70,6 +70,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.initList(msg.Width, msg.Height)
+	}
 	var cmd tea.Cmd
 	m.menuList, cmd = m.menuList.Update(msg)
 	return m, cmd

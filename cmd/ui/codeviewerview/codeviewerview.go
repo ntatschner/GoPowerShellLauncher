8d12f14ce4df9a8a7e4aed710806bfe862592734
tatux <ntatschner@gmail.com>
2024-11-30 22:23:08 +0000

    added view

--- cmd/ui/codeviewerview/codeviewerview.go
+++ cmd/ui/codeviewerview/codeviewerview.go
@@ -1,10 +1,14 @@
 package codeviewerview
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	l "github.com/ntatschner/GoPowerShellLauncher/cmd/logger"
+	"github.com/ntatschner/GoPowerShellLauncher/cmd/ui/styles"
 	"github.com/ntatschner/GoPowerShellLauncher/cmd/ui/view"
 	"github.com/ntatschner/GoPowerShellLauncher/cmd/utils"
 )
@@ -13,31 +17,47 @@ type model struct {
 	codeviewer  viewport.Model
 	profilePath string
 	viewChanger view.ViewChanger
+	windowSize  tea.WindowSizeMsg
+	help        string
 }
+
+const useHighPerformanceRenderer = false
 
 func New(path string, windowSize tea.WindowSizeMsg, viewChanger view.ViewChanger) model {
 	l.Logger.Info("Initializing code viewer")
-	ws := windowSize
-	vp := viewport.New(ws.Height, ws.Width)
 	content, err := utils.LoadProfileContent(path)
 	if err != nil {
 		l.Logger.Error("Failed to load profile content", "error", err)
+		content = "Failed to load profile content"
 	}
+	l.Logger.Info("Loaded profile content", "content", content)
+
+	title := styles.TitleStyle.Render(path)
+	vp := viewport.New(windowSize.Width, windowSize.Height)
+	line := strings.Repeat("─", max(0, vp.Width-lipgloss.Width(title)))
+	header := lipgloss.JoinHorizontal(lipgloss.Center, title, line)
+	headerHeight := lipgloss.Height(header)
+	info := styles.ViewPortInfoStyle.Render(fmt.Sprintf("%3.f%%", vp.ScrollPercent()*100))
+	footer := lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+	footerHeight := lipgloss.Height(footer)
+	help := styles.HelpStyle.Render("↑/k: up, ↓/j: down, u: ½ page up, d: ½ page down, backspace: back")
+	helpHeight := lipgloss.Height(help)
+	verticalMarginHeight := headerHeight + footerHeight + helpHeight
+	vp.YPosition = headerHeight
+	vp.HighPerformanceRendering = useHighPerformanceRenderer
 	vp.SetContent(content)
+	vp.VisibleLineCount()
+	vp.KeyMap = viewport.DefaultKeyMap()
+	vp.MouseWheelEnabled = true
+
+	vp.Height = windowSize.Height - verticalMarginHeight
 	return model{
-		codeviewer: viewport.Model{
-			Width:             ws.Width - 10,
-			Height:            ws.Height - 10,
-			MouseWheelEnabled: true,
-			Style: lipgloss.NewStyle().
-				Border(lipgloss.RoundedBorder(), true).
-				Padding(1, 1).Align(lipgloss.Center).
-				Background(lipgloss.Color("#8396a6")),
-		},
+		codeviewer:  vp,
 		profilePath: path,
 		viewChanger: viewChanger,
+		windowSize:  windowSize,
+		help:        help,
 	}
-
 }
 
 func (m model) Init() tea.Cmd {
@@ -47,12 +67,9 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.codeviewer.Height = msg.Height - 10
-		m.codeviewer.Width = msg.Width - 10
-	case tea.KeyMsg:
-		switch msg.String() {
-
-		}
+		m.windowSize = msg
+		m.codeviewer.Width = msg.Width
+		m.codeviewer.Height = msg.Height - m.codeviewer.YPosition - 1
 	}
 	var cmd tea.Cmd
 	m.codeviewer, cmd = m.codeviewer.Update(msg)
@@ -60,5 +77,18 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	return m.codeviewer.View()
+	title := styles.TitleStyle.Render(m.profilePath)
+	line := strings.Repeat("─", max(0, m.codeviewer.Width-lipgloss.Width(title)))
+	header := lipgloss.JoinHorizontal(lipgloss.Center, title, line)
+	finfo := styles.ViewPortTitleStyle.Render(fmt.Sprintf("%3.f%%", m.codeviewer.ScrollPercent()*100))
+	fline := strings.Repeat("─", max(0, m.codeviewer.Width-lipgloss.Width(finfo)))
+	footer := lipgloss.JoinVertical(lipgloss.Left, lipgloss.JoinHorizontal(lipgloss.Center, fline, finfo), m.help)
+	return lipgloss.JoinVertical(lipgloss.Left, header, m.codeviewer.View(), footer)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
--- cmd/ui/styles/styles.go
+++ cmd/ui/styles/styles.go
@@ -13,9 +13,23 @@ var (
 	ItemStyle         = lipgloss.NewStyle().PaddingLeft(4)
 	SelectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
 	PaginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
-	HelpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	HelpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(1).PaddingBottom(1).Faint(true).Align(lipgloss.Left)
 	QuitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
 )
 var BaseStyle = lipgloss.NewStyle().
 	BorderStyle(lipgloss.NormalBorder()).
 	BorderForeground(lipgloss.Color("240"))
+
+var (
+	ViewPortTitleStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Right = "├"
+		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
+	}()
+
+	ViewPortInfoStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Left = "┤"
+		return ViewPortTitleStyle.BorderStyle(b)
+	}()
+)

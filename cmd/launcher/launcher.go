ba3f3217d4fdd2e43ae36e1ab6dd5ce820d5c01e
tatux <ntatschner@gmail.com>
2024-12-03 12:37:35 +0000

    Got launcher process to detach

--- cmd/launcher/launcher.go
+++ cmd/launcher/launcher.go
@@ -47,18 +47,23 @@ func CreateTempFile(merged string) (string, error) {
 
 func ExecutePowerShellProcess(encodedCommand string, shellPath string) error {
 	l.Logger.Info("Executing PowerShell process", "ShellPath", shellPath)
-	command := fmt.Sprintf("Start-Process -FilePath %s -ArgumentList \"-NoProfile -NoExit -EncodedCommand %s\"", shellPath, encodedCommand)
+	command := fmt.Sprintf(
+		"Start-Process -FilePath \"%s\" -ArgumentList \"-NoProfile -NoExit -EncodedCommand %s\"",
+		shellPath, encodedCommand,
+	)
 	l.Logger.Info("PowerShell command", "Command", command)
-	cmd := exec.Command("cmd", "/C", "start", "/wait", "powershell", "-NoExit", "-Command", command)
+	cmd := exec.Command("cmd", "/C", "start", "/b", "/wait", "powershell", "-Command", command)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP}
+		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+		HideWindow:    true,
+	}
 
 	err := cmd.Run()
-	l.Logger.Info("PowerShell process started", "PID", cmd.Process.Pid)
 	if err != nil {
 		l.Logger.Error("Failed to start PowerShell process", "Error", err)
 		return err
 	}
+	l.Logger.Info("PowerShell process started", "PID", cmd.Process.Pid)
 	l.Logger.Info("PowerShell process started successfully")
 	return nil
 }

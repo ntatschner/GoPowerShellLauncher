1be4a749e73145c5f3f2e70ecc808d59a19471d0
tatux <ntatschner@gmail.com>
2025-02-03 12:13:16 +0000

    Refactor PowerShell command execution to use a temporary script file

--- cmd/launcher/launcher.go
+++ cmd/launcher/launcher.go
@@ -2,6 +2,8 @@ package launcher
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"syscall"
 
@@ -10,16 +12,41 @@ import (
 
 func ExecutePowerShellProcess(encodedCommand string, shellPath string) error {
 	l.Logger.Info("Executing PowerShell process", "ShellPath", shellPath)
+
+	// Create a temporary file
+	tmpFile, err := ioutil.TempFile("", "ps_script_*.ps1")
+	if err != nil {
+		l.Logger.Error("Failed to create temporary file", "Error", err)
+		return err
+	}
+	defer os.Remove(tmpFile.Name())
+
+	// Write the encoded command to the temporary file
+	scriptContent := fmt.Sprintf(
+		"$command = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String('%s'))\nInvoke-Expression $command",
+		encodedCommand,
+	)
+	if _, err := tmpFile.Write([]byte(scriptContent)); err != nil {
+		l.Logger.Error("Failed to write to temporary file", "Error", err)
+		return err
+	}
+	if err := tmpFile.Close(); err != nil {
+		l.Logger.Error("Failed to close temporary file", "Error", err)
+		return err
+	}
+
+	// Execute the PowerShell script from the temporary file
 	command := fmt.Sprintf(
-		"Start-Process -FilePath \"%s\" -ArgumentList \"-NoProfile -NoExit -EncodedCommand %s\"",
-		shellPath, encodedCommand,
+		"Start-Process -FilePath \"%s\" -ArgumentList \"-NoProfile -NoExit -File %s\"",
+		shellPath, tmpFile.Name(),
 	)
 	l.Logger.Info("PowerShell command", "Command", command)
 	cmd := exec.Command("powershell", "-Command", command)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP}
+		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+	}
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		l.Logger.Error("Failed to start PowerShell process", "Error", err)
 		return err
